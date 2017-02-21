015f8ac9d05fabe9a5b082f448911e2354c4cc5c
himanshu.agarwal <himanshu.agarwal@clairvoyantsoft.com>
2017-02-21 20:46:43 +0530

    configured view resolver for html files.

---
+++ src/main/java/com/tutoring/config/WebConfig.java
@@ -0,0 +1,32 @@
+package com.tutoring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+/**
+ * Created by himanshu.agarwal on 21-02-2017.
+ */
+
+@Configuration
+@EnableWebMvc
+public class WebConfig extends WebMvcConfigurerAdapter{
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        if (!registry.hasMappingForPattern("/static/**")) {
+            registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
+        }
+    }
+
+    @Bean
+    public InternalResourceViewResolver internalViewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/static/");
+        viewResolver.setSuffix(".html");
+        return viewResolver;
+    }
+}
---
+++ src/main/java/com/tutoring/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.tutoring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by himanshu.agarwal on 21-02-2017.
+ */
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = {"/home", "/home/"}, method = RequestMethod.GET)
+    public String home() {
+        return "index";
+    }
+}
