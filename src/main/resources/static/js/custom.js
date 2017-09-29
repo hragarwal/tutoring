9876ebc65318416b56e1b349fadb4bf333f5da13
varunmashru <varunmashru@gmail.com>
2017-09-29 13:37:52 +0530

    Lesson detail cards changed

--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -1,6 +1,8 @@
 $(document).ready(function($) {
     $(".button-collapse").sideNav();
     $(".dropdown-content").css('top', 'initial');
-	    
+	
+
+	// $('#OpenImgUpload').click(function(){ $('#imgupload').trigger('click'); });    
 });
 
