a933a7ec794e8e051d189414737458b5cd0820ef
Chirag Agrawal <chirag.agrawal@pb.com>
2017-11-16 22:11:53 +0530

    Signed-off-by: Chirag Agrawal <chirag.agrawal@pb.com> manish code

--- src/main/resources/static/js/custom.js
+++ src/main/resources/static/js/custom.js
@@ -1,8 +1,29 @@
 $(document).ready(function($) {
-    $(".button-collapse").sideNav();
-    $(".dropdown-content").css('top', 'initial');
+   // $(".button-collapse").sideNav();
+  //  $(".dropdown-content").css('top', 'initial');
 	
 
-	// $('#OpenImgUpload').click(function(){ $('#imgupload').trigger('click'); });    
+	// $('#OpenImgUpload').click(function(){ $('#imgupload').trigger('click'); });  
+	
+	var wow = new WOW({
+		mobile: false
+	});
+	wow.init();
+
+	var waypoints = $('#funfactor').waypoint(function(direction) {
+		if (direction = 'up') {
+			$(".timer").countTo({
+				speed: 5000
+			})
+		}
+	},{
+		offset: '60%'
+	});
+	
+	$(document).on('focusin', '.form-control',  function(e){
+		$(this).closest(".form-group").addClass("is-focused");
+	}).on('focusout', '.form-control',  function(e){
+		$(this).closest(".form-group").removeClass("is-focused");
+	});	
 });
 
